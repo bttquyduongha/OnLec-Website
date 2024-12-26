9112de6bcd1d2e2b2776847601abba4ae21dccaa
DESKTOP-5CFKGRE\Mr Duong <bttquyduongha@gmail.com>
2024-12-26 17:23:46 +0700

    Begin

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), // Tăng hiệu suất bằng cách giảm số lượng sự kiện thay đổi
+    provideRouter(routes), // Cung cấp định tuyến cho ứng dụng
+    provideClientHydration(withEventReplay()), provideAnimationsAsync(), // Hỗ trợ SSR (Server-Side Rendering) nếu cần
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect từ root (/) đến /home
+  { path: 'home', component: HomeComponent }, // Đường dẫn đến HomeComponent
+];
