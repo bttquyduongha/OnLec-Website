1ec11393182047bb870480184e5510cacc728d7e
DESKTOP-5CFKGRE\Mr Duong <bttquyduongha@gmail.com>
2025-02-10 16:06:36 +0700

    fix 1

--- src/app/layouts/home/howto/steps/steps.component.ts
+++ src/app/layouts/home/howto/steps/steps.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, AfterViewInit, OnDestroy, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { StepsContent } from './steps-content'; // Interface
 import { CommonModule } from '@angular/common';
 
@@ -12,14 +20,17 @@ import { CommonModule } from '@angular/common';
 export class StepsComponent implements AfterViewInit, OnDestroy {
   @Input() stepsContent!: StepsContent; // Input dữ liệu steps
   activeStep: number = 1; // Bước mặc định
+
+  @ViewChild('swiperContainer', { static: false }) swiperContainer!: ElementRef; // Truy cập Swiper qua ViewChild
+
   swiper: any; // Để quản lý instance Swiper
 
   // Lắng nghe sự kiện khi nhấn vào indicator
   setActiveStep(step: number): void {
-    // Cap nhat buoc hien tai
+    // Cập nhật bước hiện tại
     this.activeStep = step;
     if (this.swiper) {
-      // Dieu huong den slide tuong ung
+      // Điều hướng đến slide tương ứng
       this.swiper.slideTo(step - 1); // Điều hướng Swiper (index bắt đầu từ 0)
     }
   }
@@ -30,10 +41,12 @@ export class StepsComponent implements AfterViewInit, OnDestroy {
       this.activeStep = this.swiper.realIndex + 1; // Swiper index bắt đầu từ 0
     }
   }
+
   // Hàm được gọi sau khi view đã được render và Swiper đã khởi tạo
   ngAfterViewInit(): void {
-    const swiperContainer = document.querySelector('swiper-container') as any;
-    this.swiper = swiperContainer.swiper;
+    // Truy cập phần tử Swiper thông qua ViewChild
+    const swiperElement = this.swiperContainer.nativeElement;
+    this.swiper = swiperElement.swiper;
 
     if (this.swiper) {
       this.swiper.on('slideChange', this.onSlideChange.bind(this));
